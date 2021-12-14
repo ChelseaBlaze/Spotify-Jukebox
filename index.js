30aa0a3e671a7b03bc648aab83d9a11c04b1c2cd
ChelseaBlaze <81193778+ChelseaBlaze@users.noreply.github.com>
2021-12-14 15:36:11 +0100

    testpage with inline svg works

---
+++ index.js
@@ -0,0 +1,24 @@
+
+/*if (
+    document.querySelector(".nineties").getSVGDocument().getElementById("songyear")
+) {
+    console.log("songyear");
+}
+
+else {
+    console.log("nosongyear");
+} */
+                                    /*.getSVGDocument()*/
+var test = document.getElementById("nineties").contentDocument
+console.log(test);
+
+//document.querySelector(".nineties").getSVGDocument()
+//.getElementById("songyear").setAttribute("fill", "red");
+
+//document.getElementById("nineties").getSVGDocument().getElementById("songyear").setAttribute("fill", "red");
+
+//document.getElementById("songyear").setAttribute("fill", "red");
+
+
+
+console.log();
